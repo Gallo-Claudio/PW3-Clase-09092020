42d2eb30c74444cc8177966c1cc96893fc847907
Gallo <g>
2020-09-16 02:15:54 -0300

    ejemplo Generics

---
+++ Ejercicios/EjercicioGenerics/Gato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.EjercicioGenerics
+{
+    public class Gato : ISaludar
+    {
+        public string Nombre { get; set; }
+        public string Saludar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Miau Miau ");
+            sb.Append(Nombre);
+            return sb.ToString();
+        }
+    }
+}
+
